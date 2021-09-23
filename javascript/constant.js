a61e5a9d45e1f6e0e123a86ec787bbbffd49fdb1
ChrisP <petitcolaschristoper@gmail.com>
2021-09-23 21:27:30 +0200

    Début du code ( visuel provisoire )

---
+++ javascript/constant.js
@@ -0,0 +1,7 @@
+export const HTML = {
+    btns : [...document.querySelectorAll(".btn")],
+    resultat : document.querySelector(".display p"),
+    equal : document.querySelector(".special"),
+    delete : document.querySelector(".delete")
+}
+
